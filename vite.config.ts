1998a2f4693459a6ebac726cde2646579819a642
Yurii-Zelenkin <flop_i@mail.ru>
2025-10-03 17:12:16 +0500

    после успешного деплоя вернул конфинурацииn

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/store_vegetable/",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
 });
